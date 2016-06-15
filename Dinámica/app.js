3a2e2a8f7fcba8c779ce5e56772e9241495fbd9d
Antonio Paya <antonioalfa22@gmail.com>
2016-06-15 16:11:43 +0200

    V.1.1 (Paso de html a jade paso 1)

--- Dinámica/app.js
+++ Dinámica/app.js
@@ -4,6 +4,7 @@ var jade = require('jade');
 var app = express();
 
 app.set("view engine","jade");
+app.use(express.static("public"));
 app.get("/",function(solicitud,respuesta){
 	// "/" significa que acepta las url que sean del tipo localhost:8080/ o sin barra, no /app por ej
 	// get es un metodo de HTTP como post o get. Post=formularios
