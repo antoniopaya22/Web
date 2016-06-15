8574a2694b177967262d6b25fb3e9f14466f0c48
Antonio Paya <antonioalfa22@gmail.com>
2016-06-15 15:15:18 +0200

    V.1.1 (Vistas con Jade)

--- Dinámica/app.js
+++ Dinámica/app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var jade = require('jade');
+
 var app = express();
 
-app.get("/",function(req,res){
-
-	res.end("Hola Mundo");
-
+app.set("view engine","jade");
+app.get("/",function(solicitud,respuesta){
+	// "/" significa que acepta las url que sean del tipo localhost:8080/ o sin barra, no /app por ej
+	// get es un metodo de HTTP como post o get. Post=formularios
+	//respuesta.send("Hola Mundo");
+	respuesta.render("index");
 });
 
 app.listen(8080);
